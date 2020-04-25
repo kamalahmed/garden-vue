a60a25f52edda9d2cefbc96b62b2c3bf78c80fd7
Kamal Ahmed <kamalacca@gmail.com>
2020-04-25 21:48:45 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    fontFamily: {
+      body: ['Roboto', 'sans-serif']
+    },
+    fontSize: {
+      xs: ['13px'],
+      sm: ['14px'],
+      tiny: ['15px'],
+      base: ['16px'],
+      semibase: ['18px'],
+      medium: ['20px'],
+      mediumbase: ['28px'],
+      lg: ['38px', '48px'],
+      xlg: ['60px', '68px'],
+    },
+    extend: {
+      colors: {
+        primary: '#353535',
+        secondary: '#74b814',
+        ternary: '#8c7a73',
+        lighter: '#f5f5f5',
+        greendark: '#578a0f',
+        greenlight: '#F1F6EA',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+};
