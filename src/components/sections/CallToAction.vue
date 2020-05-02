e7ef9b09adaa74166351297cfb39b6bbcc157233
Kamal Ahmed <kamalacca@gmail.com>
2020-05-02 18:07:44 +0600

    made call to action section responsive

--- src/components/sections/CallToAction.vue
+++ src/components/sections/CallToAction.vue
@@ -1,10 +1,10 @@
 <template>
 <section class="cta-area pt-space5 pb-space5 text-center">
-    <div class="container">
+    <div class="container md:w-space19">
         <div class="section-heading">
-            <h2 class="text-primary font-bold text-4xxl pb-5" v-if="content.title" v-html="content.title">
+            <h2 class="text-primary font-bold text-4xxl md:text-2xxl pb-5" v-if="content.title" v-html="content.title">
             </h2>
-            <h3 class="text-secondary font-bold text-4xxl pb-6" v-if="content.subtitle">{{content.subtitle}}</h3>
+            <h3 class="text-secondary font-bold text-4xxl md:text-2xxl pb-6" v-if="content.subtitle">{{content.subtitle}}</h3>
         </div><!-- end section-heading -->
         <div class="flex justify-center pb-5">
             <Button :text="content.buttonText" :link="content.buttonLink" />
